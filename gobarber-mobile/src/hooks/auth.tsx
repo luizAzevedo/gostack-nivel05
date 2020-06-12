6db5bd5ca0cb913003695773449a3a14594c8e21
luizAzevedo <lfaazevedo@hotmail.com>
2020-06-11 22:58:29 -0300

    Finalizando front-end mobile do app - Dashboard - Buscando providers da API

--- gobarber-mobile/src/hooks/auth.tsx
+++ gobarber-mobile/src/hooks/auth.tsx
@@ -61,6 +61,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -81,6 +83,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
